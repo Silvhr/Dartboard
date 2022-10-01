7a9a9536e2db4d07a832ef45046a2e3c941ff5ff
Silvhr <silvhr@protonmail.com>
2022-10-01 12:45:19 -0500

    removed template assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <h2>Github Pages</h2>
+  <h3>Deploying React to Github</h3>
+  </div>
   );
 }
 
+const About = () => {
+ return ( 
+ <div>
+ <Link to="/">Home</Link>
+ <h2>About Page</h2> 
+ </div>
+ );
+}
+
+export default About;
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import About from "./About";
+ReactDOM.render(
+ <React.StrictMode>
+ <HashRouter>
+ <Routes>
+ <Route path="/" element={<App/>} />
+ <Route path="/about" element={<About/>}/>
+ </Routes>
+ </HashRouter>
+ </React.StrictMode>,
+ document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
